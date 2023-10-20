64e00f17127c8431b6644283a9f06b6be92e1acf
dave <davidmasq@gmail.com>
2023-10-19 17:34:04 -0700

    finished (?) instructions 8x-Bx. maybe. carry/half carry flags are annoying. started work on 0x-3x area a little. Other random cleanup and stuff.

--- logic.go
+++ logic.go
@@ -5,23 +5,72 @@ import (
 )
 
 var (
-	outOfBounds error = errors.New("program counter out of bounds")
+	outOfBounds     error = errors.New("program counter out of bounds")
+	executionHalted error = errors.New("execution halted. probably HALT or STOP")
 )
 
 // decode/execute the instruction.
 func (g *GB) execute(inst byte) error {
 
-	// high/low bits of instruction to switch on
+	//16-bit instruction. fetch the next byte and switch on that
+	if inst == 0xCB {
+		// inst2, err := g.fetch(g.PC + 1)
+		// if err != nil {
+		// 	return err
+		// }
+		// _low := inst2 & 0x0F
+		// _high := inst2 >> 4
+		//TODO: implement this
+
+	}
+	// high/low nibbles of instruction to switch on
 	_low := inst & 0x0F
 	_high := inst >> 4
 
 	switch _high {
 
-	// mostly LD instructions
+	//first 4 rows, jumble of instructions organized into columns of 4
+	case 0x0, 0x1, 0x2, 0x3:
+		switch _low {
+		case 0x0:
+			switch inst {
+			case 0x00: //NOP
+				g.Cycle++
+			case 0x10: //STOP
+				return executionHalted
+			case 0x20: //TODO
+			case 0x30: //TODO
+			}
+		case 0x1: //LD d16 into target
+			operand, err := g.doubleFetch(g.PC + 1)
+			if err != nil {
+				return err
+			}
+			var lowTarget, highTarget *byte
+			switch _high {
+			case 0x0:
+				highTarget, lowTarget = &g.B, &g.C
+			case 0x1:
+				highTarget, lowTarget = &g.D, &g.E
+			case 0x2:
+				highTarget, lowTarget = &g.H, &g.L
+			case 0x3:
+				//this is done different because stack pointer is one 16bit address instead of pairs
+				g.SP = operand
+				g.Cycle++
+				return nil
+			}
+			*highTarget = byte(operand >> 8)
+			*lowTarget = byte(operand & 0x00FF)
+			g.Cycle++
+		}
+
+	// LD instructions
 	// 0x76 is HALT
 	case 0x4, 0x5, 0x6, 0x7:
 		if inst == 0x76 {
 			//halt TODO: probably just return an error w/e
+			return executionHalted
 		}
 		var target *byte
 		operand := g.getOperand(_low)
@@ -47,7 +96,7 @@ func (g *GB) execute(inst byte) error {
 		case 0x7:
 			if _low < 0x8 {
 				//HL fetching requires another cycle
-				g.Timer++
+				g.Cycle++
 				addr := getPair(g.H, g.L)
 				target = &g.WMem[addr]
 			} else {
@@ -55,46 +104,66 @@ func (g *GB) execute(inst byte) error {
 			}
 		}
 		*target = operand
-		g.Timer++
+		g.Cycle++
 	//0x8 ADD/ADC
 	case 0x8:
 		operand := g.getOperand(_low)
 
 		//if _low bit < 0x8 = ADD, otherwise ADC
 		g.addWithMaybeCarry(operand, (_low < 0x8))
-		g.Timer++
+		g.Cycle++
 	//0x9 SUB/SBC
 	case 0x9:
 		operand := g.getOperand(_low)
 
 		//if _low bit < 0x8 = SUB, otherwise SBC
 		g.subWithMaybeCarry(operand, (_low < 0x8))
-		g.Timer++
+		g.Cycle++
 	//0xA AND/XOR
 	case 0xA:
+		operand := g.getOperand(_low)
+
+		if _low < 0x8 {
+			g.andA(operand)
+		} else {
+			g.xorA(operand)
+		}
+		g.Cycle++
 
 	//0xB OR/CP
 	case 0xB:
+		operand := g.getOperand(_low)
+
+		if _low < 0x8 {
+			g.orA(operand)
+		} else {
+			g.cpA(operand)
+		}
+		g.Cycle++
 	}
 
 	return nil
 }
 
 // returns byte at address
+// adds 1 cycle TODO: probably?
 func (g *GB) fetch(addr uint16) (byte, error) {
 	if g.PC < 0 || int(g.PC)+1 > len(g.WMem) {
 		return 0, outOfBounds
 	}
+	g.Cycle++
 	return g.WMem[addr], nil
 }
 
 // returns 2 bytes at address
-// TODO: probably flip the bytes. wee endian
+// adds 2 cycles
+// returns in flipped order because little-endian
 func (g *GB) doubleFetch(addr uint16) (uint16, error) {
 	if g.PC < 0 || int(g.PC)+2 > len(g.WMem) {
 		return 0, outOfBounds
 	}
-	return uint16(uint16(g.WMem[addr])<<8 | uint16(g.WMem[addr+1])), nil
+	g.Cycle += 2
+	return uint16(uint16(g.WMem[addr+1])<<8 | uint16(g.WMem[addr])), nil
 }
 
 // smoosh 2 bytes into uint16
@@ -103,7 +172,7 @@ func getPair(MostSig, LeastSig byte) uint16 {
 	return uint16(uint16(MostSig)<<8 | uint16(LeastSig))
 }
 
-// get the operand for instructions 4x-Bx
+// get the operand for a chunk of instructions
 // may increase timer for HL access
 func (g *GB) getOperand(op byte) byte {
 	var operand byte
@@ -122,7 +191,7 @@ func (g *GB) getOperand(op byte) byte {
 		operand = g.L
 	case 0x6, 0xE:
 		//HL fetching requires another cycle
-		g.Timer++
+		g.Cycle++
 		addr := getPair(g.H, g.L)
 		operand = g.WMem[addr]
 	case 0x7, 0xF:
@@ -135,7 +204,7 @@ func (g *GB) getOperand(op byte) byte {
 // may increase timer with HL access
 func (g *GB) addWithMaybeCarry(inp byte, ADC bool) {
 	//input is 1 more if ADC
-	if ADC {
+	if ADC && g.F.CY {
 		inp++
 	}
 	//half-carry and carry have to be set first
@@ -151,7 +220,7 @@ func (g *GB) addWithMaybeCarry(inp byte, ADC bool) {
 }
 
 func (g *GB) subWithMaybeCarry(inp byte, SBC bool) {
-	if SBC {
+	if SBC && g.F.CY {
 		inp++
 	}
 	g.F.CY = (int16(g.A-inp) < 0) //lazy borrow check
@@ -163,4 +232,54 @@ func (g *GB) subWithMaybeCarry(inp byte, SBC bool) {
 
 	g.F.Z = (g.A == 0) //zero flag
 	g.F.N = true       //N is always set
+}
+
+// boolean AND input byte with register A
+func (g *GB) andA(inp byte) {
+	g.A = g.A & inp
+
+	//set flags
+	g.F.Z = (g.A == 0)
+	g.F.N = false
+	g.F.H = true
+	g.F.CY = false
+}
+
+// boolean XOR input byte with register A
+func (g *GB) xorA(inp byte) {
+	g.A = g.A ^ inp
+
+	//set flags
+	g.F.Z = (g.A == 0)
+	g.F.N = false
+	g.F.H = false
+	g.F.CY = false
+
+}
+
+// boolean OR input byte with register A
+func (g *GB) orA(inp byte) {
+	g.A = g.A | inp
+
+	//set flags
+	g.F.Z = (g.A == 0)
+	g.F.N = false
+	g.F.H = false
+	g.F.CY = false
+
+}
+
+// boolean CP input byte with register A
+func (g *GB) cpA(inp byte) {
+	result := g.A - inp
+
+	// flagsssss
+	g.F.Z = (result == 0)
+	g.F.N = false
+
+	// carry/half-carry just stolen from SBC which is already questionable
+	// TODO: maybe verify them?
+	g.F.CY = (int16(g.A-inp) < 0)
+	g.F.H = ((g.A & 0x0F) < (inp & 0x0F))
+
 }
--- main.go
+++ main.go
@@ -7,19 +7,20 @@ import (
 )
 
 const (
-	pxSize int32 = 2
+	pxSize int32 = 4
 	xRes   int32 = 166 * pxSize
 	yRes   int32 = 144 * pxSize
 )
 
-// figure out a better way to use registers.
-// they need to be able to be combined and mutated together.
+// gameboy state.
 type GB struct {
 	A, B, C, D, E, H, L byte         //general registers
 	F                   flagRegister //carry flags and stuff
 	PC, SP              uint16       //program counter, stack pointer
 	WMem                [8192]byte   //workram!
-	Timer               uint32       //cycle timer
+	//gameboy is 4mhz and every operation is a multiple of 4
+	//simplify in units of 1
+	Cycle uint32 //cycle timer
 }
 
 // this is easier than doing bitmasking bleh
@@ -31,6 +32,7 @@ type PPU struct {
 	VRAM *[8192]byte //vram!
 }
 
+// initialize everything
 func newGB() (*GB, error) {
 	return &GB{
 		SP: 0xFFFE,
@@ -48,7 +50,13 @@ func (g *GB) Update() error {
 	}
 	return nil
 }
+
+// implement everything eventually
 func (g *GB) Draw() error {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Gray)
+
+	rl.EndDrawing()
 	return nil
 }
 
